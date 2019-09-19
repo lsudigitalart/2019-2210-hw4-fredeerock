86f8a0958498a5ed5fcf1d3bb8a6f1b90a6e4f5b
Derick Ostrenko <dostrenko@gmail.com>
2019-09-19 14:51:54 -0500

    game

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
 function setup() {
-
+  createCanvas(800, 600);
 }
 
+var topLeftCorner = 0;
+var topRightCorner = 0;
+
 function draw() {
+  background(100);
+  circle(mouseX, mouseY, 100);
+
+  if (mouseX < 0 && mouseY < 0) {
+    topLeftCorner = 1;
+    print("tlc");
+  }
+
+  if (mouseX > width && mouseY < 0) {
+    topRightCorner = 1;
+    print("trc");
+  }
+
+  if (topLeftCorner == 1 && topRightCorner == 1) {
+    background("green");
+  }
 
 }
