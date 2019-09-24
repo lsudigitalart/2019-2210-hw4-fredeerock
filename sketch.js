fc4777f3a59007944f3ca37952e572823f8824d4
Derick Ostrenko <dostrenko@gmail.com>
2019-09-24 13:06:31 -0500

    fonts!

--- sketch.js
+++ sketch.js
@@ -1,13 +1,22 @@
-function setup() {
-  createCanvas(800, 600);
-}
+var backColor;
 
 var topLeftCorner = 0;
 var topRightCorner = 0;
+var score = 0;
+
+var scoreFont;
+
+function preload() {
+  scoreFont = loadFont("mansalva.ttf");
+}
+
+function setup() {
+  createCanvas(800, 600);
+  backColor = color("grey");
+}
 
 function draw() {
   background(100);
-  circle(mouseX, mouseY, 100);
 
   if (mouseX < 0 && mouseY < 0) {
     topLeftCorner = 1;
@@ -19,8 +28,21 @@ function draw() {
     print("trc");
   }
 
-  if (topLeftCorner == 1 && topRightCorner == 1) {
-    background("green");
+  if (topLeftCorner == 1 || topRightCorner == 1) {
+    score = "500,000";
+    backColor = "brown";
   }
+
+  if (topLeftCorner == 1 && topRightCorner == 1) {
+    score = "1,000,000";
+    backColor = "green";
+  }
+
+  background(backColor);
+  textAlign(CENTER);
+  textFont(scoreFont);
+  textSize(50);
+  text(score, width / 2, height / 2);
+  circle(mouseX, mouseY, 100);
 
 }
